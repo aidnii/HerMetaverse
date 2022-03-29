8c0415f9a5b315584ee8fddc2fffbc2819c577fa
cryptichic <syberchief@gmail.com>
2022-03-29 16:02:14 -0500

    created contract land and identified it as NFT ERC 721

--- hermetaverse/src/contracts/Land.sol
+++ hermetaverse/src/contracts/Land.sol
@@ -1,5 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+contract Land is ERC721 {
+    uint256 public cost = 1 ether;
+    uint256 public maxSupply = 5;
+    uint256 totalSupply = 0;
+
+    struct Building {
+        string name;
+        address owner;
+        int256 posX;
+        int256 posY;
+        int256 posZ;
+        uint256 sizeX;
+        uint256 sizeY;
+        uint256 sizeZ;
+    }
+
+    
+}
 
