e03177215c54504195bd972b0b8635f0b4ee71fc
cryptichic <syberchief@gmail.com>
2022-03-29 16:04:07 -0500

    added constructor with variables added building arrays, added stadium, university, shopping plaza's 1 and 2

--- hermetaverse/src/contracts/Land.sol
+++ hermetaverse/src/contracts/Land.sol
@@ -19,6 +19,38 @@ contract Land is ERC721 {
         uint256 sizeZ;
     }
 
+    Building[] public buildings;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _cost
+    ) ERC721(_name, _symbol) {
+        cost = _cost;
+        // adding the City Hall building
+        buildings.push(
+            Building("City Hall", address(0x0), 0, 0, 0, 10, 10, 10)
+        );
+
+        // adding the Stadium building
+        buildings.push(Building("Stadium", address(0x0), 0, 10, 0, 10, 5, 3));
+
+        // adding the University building
+        buildings.push(
+            Building("University", address(0x0), 0, -10, 0, 10, 5, 3)
+        );
+
+        // adding the Shopping Plaza 1 building
+        buildings.push(
+            Building("Shopping Plaza 1", address(0x0), 10, 0, 0, 5, 25, 5)
+        );
+
+        // adding the Shopping Plaza 2 building
+        buildings.push(
+            Building("Shopping Plaza 2", address(0x0), -10, 0, 0, 5, 25, 5)
+        );
+    }
+
     
 }
 
