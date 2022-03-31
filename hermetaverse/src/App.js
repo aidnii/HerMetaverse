14e5f64d163db9f81a9927ee7446cacb853c4998
cryptichic <syberchief@gmail.com>
2022-03-31 16:33:54 -0500

    adding light and sky distance characteristics

--- hermetaverse/src/App.js
+++ hermetaverse/src/App.js
@@ -77,7 +77,11 @@ function App() {
   return (
     <div>
       <Navbar web3Handler={web3Handler} account={account} />
-      <Canvas camera={{ position: [0, 0, 30], up: [0, 0, 1], far: 10000 }}></Canvas>
+      <Canvas camera={{ position: [0, 0, 30], up: [0, 0, 1], far: 10000 }}>
+        <Sky distance={450000} sunPosition={[1, 10, 0]} inclination={0} azimuth={0.25} />
+
+        <ambientLight intensity={0.5} />
+      </Canvas>
     </div>
   );
 }
