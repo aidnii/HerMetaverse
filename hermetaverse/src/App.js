9fc41bd7131cd991f3e6fde504bf4c03d61bdcb7
cryptichic <syberchief@gmail.com>
2022-03-30 21:30:47 -0500

    setting up app.js file

--- hermetaverse/src/App.js
+++ hermetaverse/src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+import { useState, useEffect } from 'react';
+
+import Land from './abis/Land.json';
 
 function App() {
+
+  const [web3,setWeb3] = useState(null);
+  const [account,setAccount] = useState(null);
+  const [landContract,setLandContract] = useState(null);
+  const [cost,setCost] = useState(null);
+  const [buildings,setBuildings] = useState(null);
+
+  useEffect( () => {
+    loadBlockchainData()
+  }, [account])
+
+  const loadBlockchainData = async () => {
+    if(typeof window.ethereum !== 'undefined') {
+      const web3 = new Web3(window.ethereum)
+      setWeb3(web3)
+
+      const accounts = await web3.eth.getAccounts();
+
+      if(account.length > 0) {
+        setAccount(accounts[0])
+      }
+
+      const networkId = await web3.eth.net.getId()
+
+      const land = new web3.eth.Contract(Land.abi, Land.networks[networkId].address)
+      setLandContract(land)
+
+      const cost = await land.methods.cost().call()
+      setCost(web3.utils.fromWei(cost.toString(), 'ether'))
+
+      const buildings = await land.methods.getBuildings().call()
+      setBuildings(buildings)
+
+      // adding event listener - recommended from metamask api
+      window.ethereum.on('accountsChanged', function (accounts) {
+        setAccount(accounts[0])
+      })
+
+      // adding event listener 
+      window.ethereum.on('chainChanged', (chainId) => {
+        window.location.reload();
+      }
+    
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Her Virtual Land
     </div>
   );
 }
